1383f4162c2a702920287b8729bf561d391e94cd
Vitaly Babiy <vbabiy86@gmail.com>
2014-07-17 14:15:22 -0400

    Fix relative imports for python 3. Fixes #7.

--- djangorestframework_camel_case/__init__.py
+++ djangorestframework_camel_case/__init__.py
@@ -5,5 +5,5 @@ __author__ = 'Vitaly Babiy'
 __email__ = 'vbabiy86@gmail.com'
 __version__ = '0.1.3'
 
-from render import CamelCaseJSONRenderer
-from parser import CamelCaseJSONParser
+from .render import CamelCaseJSONRenderer
+from .parser import CamelCaseJSONParser
