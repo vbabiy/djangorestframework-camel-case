dac4987bfb3c7c1833b70ddc5615e6728bf10a13
Yavor Atov <yavor.atov@gmail.com>
2014-10-31 15:10:23 +0200

    Make the renderer/parser superclass configurable

--- djangorestframework_camel_case/render.py
+++ djangorestframework_camel_case/render.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from rest_framework.renderers import JSONRenderer
+from djangorestframework_camel_case.settings import api_settings
 from djangorestframework_camel_case.util import camelize
 
 
-class CamelCaseJSONRenderer(JSONRenderer):
+class CamelCaseJSONRenderer(api_settings.RENDERER_CLASS):
     def render(self, data, *args, **kwargs):
         return super(CamelCaseJSONRenderer, self).render(camelize(data), *args,
                                                          **kwargs)
