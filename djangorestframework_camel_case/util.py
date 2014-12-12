cca44f19d83d164f71702adb050fdd463fa951af
Hannes Schippmann <hannes@virtusize.com>
2014-12-12 15:44:06 +0100

    Bugfix for camelize/underscoreize
    The camelize and underscoreize functions were changing the actual data
    that was passed in. This led to the problem that the returning
    Response.data was containing camelized keys.
    
    The data attribute of Response should instead always contain the
    unrendered data.
    
    I also added some more tests to make sure it behaves like it should.

--- djangorestframework_camel_case/util.py
+++ djangorestframework_camel_case/util.py
@@ -1,42 +1,33 @@
+import inflection
 from collections import OrderedDict
-import re
-
-first_cap_re = re.compile('(.)([A-Z][a-z]+)')
-all_cap_re = re.compile('([a-z0-9])([A-Z])')
-
-
-def underscoreToCamel(match):
-    return match.group()[0] + match.group()[2].upper()
 
 
 def camelize(data):
     if isinstance(data, dict):
         new_dict = OrderedDict()
-        for key, value in data.items():
-            new_key = re.sub(r"[a-z]_[a-z]", underscoreToCamel, key)
-            new_dict[new_key] = camelize(value)
+
+        for k, v in data.items():
+            new_dict[inflection.camelize(k, uppercase_first_letter=False)] = camelize(v)
+
         return new_dict
-    if isinstance(data, (list, tuple)):
-        for i in range(len(data)):
-            data[i] = camelize(data[i])
-        return data
+
+    if isinstance(data, list):
+        return [camelize(x) for x in data]
+
+    if isinstance(data, tuple):
+        return tuple(camelize(x) for x in data)
+
     return data
-
-
-def camel_to_underscore(name):
-    s1 = first_cap_re.sub(r'\1_\2', name)
-    return all_cap_re.sub(r'\1_\2', s1).lower()
 
 
 def underscoreize(data):
     if isinstance(data, dict):
-        new_dict = {}
-        for key, value in data.items():
-            new_key = camel_to_underscore(key)
-            new_dict[new_key] = underscoreize(value)
-        return new_dict
-    if isinstance(data, (list, tuple)):
-        for i in range(len(data)):
-            data[i] = underscoreize(data[i])
-        return data
+        return {inflection.underscore(k): underscoreize(v) for k, v in data.items()}
+
+    if isinstance(data, list):
+        return [underscoreize(x) for x in data]
+
+    if isinstance(data, tuple):
+        return tuple(underscoreize(x) for x in data)
+
     return data
