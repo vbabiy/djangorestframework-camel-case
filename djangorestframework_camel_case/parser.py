17410636fc22d9c5179022a20cd2eeaf5179221f
Daniel Gilge <33256939+dgilge@users.noreply.github.com>
2018-06-13 14:30:58 +0200

    Add underscore config as class attribute

--- djangorestframework_camel_case/parser.py
+++ djangorestframework_camel_case/parser.py
@@ -9,15 +9,14 @@ from djangorestframework_camel_case.util import underscoreize
 
 
 class CamelCaseJSONParser(api_settings.PARSER_CLASS):
+    'json_underscoreize' = api_settings.JSON_UNDERSCOREIZE
+
     def parse(self, stream, media_type=None, parser_context=None):
         parser_context = parser_context or {}
         encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
 
         try:
             data = stream.read().decode(encoding)
-            return underscoreize(
-                json.loads(data),
-                **api_settings.JSON_UNDERSCOREIZE
-            )
+            return underscoreize(json.loads(data), **self.json_underscoreize)
         except ValueError as exc:
             raise ParseError('JSON parse error - %s' % six.text_type(exc))
