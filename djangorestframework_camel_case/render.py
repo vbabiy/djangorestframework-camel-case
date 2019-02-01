8c651855911664a97616f6c7abae0b40cfa1709e
insung151 <insung151@naver.com>
2019-02-01 22:49:52 +0900

    Add camel case browsable renderer

--- djangorestframework_camel_case/render.py
+++ djangorestframework_camel_case/render.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from rest_framework.renderers import BrowsableAPIRenderer
+
 from djangorestframework_camel_case.settings import api_settings
 from djangorestframework_camel_case.util import camelize
 
@@ -6,5 +8,12 @@ from djangorestframework_camel_case.util import camelize
 class CamelCaseJSONRenderer(api_settings.RENDERER_CLASS):
     def render(self, data, *args, **kwargs):
         return super(CamelCaseJSONRenderer, self).render(
+            camelize(data), *args, **kwargs
+        )
+
+
+class CamelCaseBrowsableAPIRenderer(BrowsableAPIRenderer):
+    def render(self, data, *args, **kwargs):
+        return super(CamelCaseBrowsableAPIRenderer, self).render(
             camelize(data), *args, **kwargs
         )
