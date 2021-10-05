09bfd850d7d56e1fd53bfcd3e36e1a4e7c2fec3b
sergiusnick <sergiusnick@yandex-team.ru>
2021-10-05 19:55:47 +0500

    CamelCaseJSONRenderer json_underscoreize

--- djangorestframework_camel_case/render.py
+++ djangorestframework_camel_case/render.py
@@ -5,9 +5,11 @@ from djangorestframework_camel_case.util import camelize
 
 
 class CamelCaseJSONRenderer(api_settings.RENDERER_CLASS):
+    json_underscoreize = api_settings.JSON_UNDERSCOREIZE
+
     def render(self, data, *args, **kwargs):
         return super().render(
-            camelize(data, **api_settings.JSON_UNDERSCOREIZE), *args, **kwargs
+            camelize(data, **self.json_underscoreize), *args, **kwargs
         )
 
 
